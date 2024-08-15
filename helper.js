b87b746b129b3f276ee14076252cf5ab03938e51
Muhammad Royyan <pandu.royyan@zetta-intern.space>
2024-08-15 08:49:11 +0700

    javascript advance day 4

---
+++ helper.js
@@ -0,0 +1,40 @@
+const calculateCredit = (term, totalPrice) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let credits = [];
+      let currentDate = new Date();
+      for (let i = 1; i <= term; i++) {
+        let dueDate = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth() + i,
+          currentDate.getDate()
+        );
+        let credit = {
+          dueDate: dueDate.toISOString().split("T")[0],
+          amount: Math.round(calculateInterestPrice(term, totalPrice) / term),
+        };
+
+        credits.push(credit);
+      }
+
+      resolve(credits);
+    }, 200);
+  });
+};
+
+const calculateInterestPrice = (term, priceStart) => {
+  let interestRate = 0;
+
+  if (term <= 3) {
+    interestRate = 10;
+  } else {
+    interestRate = 10;
+    let additionalPeriods = Math.ceil((term - 3) / 3);
+    interestRate += additionalPeriods * 5;
+  }
+
+  let interestPrice = (priceStart * interestRate) / 100;
+  return interestPrice + priceStart;
+};
+
+export { calculateCredit, calculateInterestPrice };
---
+++ server.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { books } from "./books.js";
+import { calculateCredit, calculateInterestPrice } from "./helper.js";
+import { auth } from "./middlewares/auth.js";
+
+// PARAMETERS
+const DISCOUNT = 20;
+const TAX = 10;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/purchase", auth, async (req, res) => {
+  const { bookID, term, quantity } = req.body;
+
+  let book = books.find((book) => book.id === bookID);
+  if (!book) {
+    return res
+      .status(404)
+      .json({ message: `Cannot find book with id ${bookID}` });
+  }
+
+  if (quantity > book.stock) {
+    return res
+      .status(400)
+      .json({ message: `Maximum purchase is ${book.stock}` });
+  }
+
+  let priceStart = quantity * book.price;
+  let discountAmount = (priceStart * DISCOUNT) / 100;
+  let taxAmount = (priceStart * TAX) / 100;
+
+  let normalPrice = priceStart - discountAmount + taxAmount;
+
+  let allTerms = await calculateCredit(term, normalPrice);
+  let distinctTerm = new Set();
+  let termByDate = new Map();
+  allTerms.forEach((term) => {
+    distinctTerm.add(term.amount);
+    termByDate.set(term.dueDate, { amount: term.amount, paid: false });
+  });
+
+  // hardcode one term data to get paid
+  let entry = termByDate.get(allTerms[0].dueDate);
+  entry.paid = true;
+  termByDate.set(allTerms[0].dueDate, entry);
+
+  return res.status(200).json({
+    normalPrice: normalPrice,
+    priceWithCredit: calculateInterestPrice(term, normalPrice),
+    allTerms: allTerms,
+    distinctTerm: Array.from(distinctTerm),
+    termByDate: Array.from(termByDate, ([key, value]) => ({ [key]: value })),
+  });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
