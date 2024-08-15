c3e5a22f23abcf0c35c92d702ecf6b8f3d996712
Muhammad Royyan <pandu.royyan@zetta-intern.space>
2024-08-15 09:00:05 +0700

    mongodb day 2

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bookRoutes = require("./routes/bookRoutes.js");
+
+mongoose
+  .connect("mongodb://localhost:27017/bookstore", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bookRoutes);
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
